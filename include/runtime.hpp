76e803b3517b95847b106f46ab7bb7f311c1d48e
xNoerPlaysCodes <carishma.akhauri81@gmail.com>
2025-06-21 20:11:08 +0530

    dev-open

--- include/runtime.hpp
+++ include/runtime.hpp
@@ -7,7 +7,7 @@
 namespace rocket {
     #define ROCKETGE_MINOR_VERSION  "1"
     #define ROCKETGE_MAJOR_VERSION  "0"
-    #define ROCKETGE_BUILD          "dev"
+    #define ROCKETGE_BUILD          "open-dev"
     #define ROCKETGE_VERSION        ROCKETGE_MAJOR_VERSION "." ROCKETGE_MINOR_VERSION "-" ROCKETGE_BUILD
 }
 #define ROCKETGE_OpenGL_SupportComputeShader
