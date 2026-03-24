66da8be49a49416ea5f17d29cf77525d0ecb6987
xNoerPlaysCodes <carishma.akhauri81@gmail.com>
2026-03-24 18:35:45 +0530

    runtime: Set default main thread name to RocketGE instead of Main Thread to avoid confusion

--- src/rocket/core/runtime.cpp
+++ src/rocket/core/runtime.cpp
@@ -125,7 +125,7 @@ void __init() {
     rocket::log("Hooked SIGBUS, SIGSEGV, SIGIOT, SIGABRT", "rocket", "init", "debug");
     
     util::init_memory_buffer();
-    rnative::set_thread_name("Main Thread");
+    rnative::set_thread_name("RocketGE");
 
     rocket::log("Emergency memory buffer initialized with size " + std::format("{} MiB", util::get_memory_buffer()->sz / 1024 / 1024), "rocket", "init", "debug");
 }
