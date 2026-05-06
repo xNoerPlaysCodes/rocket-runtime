cd4836bf5873800443cf0f7ed9c9f7c8cbd700e9
xNoerPlaysCodes <carishma.akhauri81@gmail.com>
2026-05-06 13:41:39 +0530

    Make crashdump.hpp public header

---
+++ include/rocket/crashdump.hpp
@@ -0,0 +1,12 @@
+#ifndef ROCKETGE__CRASHDUMP_HPP
+#define ROCKETGE__CRASHDUMP_HPP
+
+namespace rocket {
+    /// @brief Invalidates previous crash messages on invocation
+    char* crash_signal(bool fatal, void *mem_addr, const char *signal, const char *message);
+
+    /// @brief Java-like stack trace + exit function
+    [[noreturn]] void crash_with_stacktrace() noexcept;
+}
+
+#endif//ROCKETGE__CRASHDUMP_HPP
--- src/include/crashdump.hpp
+++ src/include/crashdump.hpp
@@ -1,14 +1,1 @@
-#ifndef ROCKETGE__CRASHDUMP_HPP
-#define ROCKETGE__CRASHDUMP_HPP
-
-namespace rocket {
-    struct crash_info_t {};
-
-    /// @brief Invalidates previous crash messages on invocation
-    char* crash_signal(bool fatal, void *mem_addr, const char *signal, const char *message);
-
-    /// @brief Java-like stack trace + exit function
-    [[noreturn]] void crash_with_stacktrace() noexcept;
-}
-
-#endif//ROCKETGE__CRASHDUMP_HPP
+#include "rocket/crashdump.hpp"
